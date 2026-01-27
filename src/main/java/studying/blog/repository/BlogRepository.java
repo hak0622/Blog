36d5ca8d0645049445a78cc4768e1f7963e23fea
hak0622 <seounghak062@naver.com>
2026-01-27 17:34:28 +0900

    refactor: BlogRepository 조회 성능 개선(EntityGraph 적용)

--- src/main/java/studying/blog/repository/BlogRepository.java
+++ src/main/java/studying/blog/repository/BlogRepository.java
@@ -1,6 +1,9 @@
 package studying.blog.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import studying.blog.domain.Article;
@@ -8,4 +11,7 @@ import studying.blog.domain.Article;
 import java.util.List;
 
 public interface BlogRepository extends JpaRepository<Article,Long> {
+    @Override
+    @EntityGraph(attributePaths = {"author"})
+    Page<Article> findAll(Pageable pageable);
 }
